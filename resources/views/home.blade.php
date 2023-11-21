f987918c4354d9de5ba992e837930678c9510d33
R-G2110 <argeldelacruz86@gmail.com>
2023-11-21 21:10:40 +0100

    [Add]: table on home.blade.php

---
+++ resources/views/home.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.main')
+
+@section('content')
+
+    <div class="container">
+
+        <table class="table table-bordered border-primary">
+            <thead>
+              <tr>
+                <th scope="col">Treno</th>
+                <th scope="col">Partenza da</th>
+                <th scope="col">Ore</th>
+                <th scope="col">Arrivo a</th>
+                <th scope="col">Ore</th>
+                <th scope="col">N° carrozze</th>
+                <th scope="col">Stato</th>
+
+              </tr>
+            </thead>
+            <tbody>
+                @foreach ($trains as $train)
+                    <tr>
+                    <th scope="row">{{ $train->company }}</th>
+                    <td>{{ $train->departure_station }}</td>
+                    <td>{{ $train->departure_time }}</td>
+                    <td>{{ $train->arrival_station }}</td>
+                    <td>{{ $train->arrival_time }}</td>
+                    <td>{{ $train->wagons }}</td>
+                    <td>
+                        @if ($train->cancelled)
+                            <p>Cancellato</p>
+                        @elseif ($train->in_time)
+                            <p>In orario</p>
+                        @else
+                            <p>In ritardo di </p>
+                        @endif
+                    </td>
+                    </tr>
+                @endforeach
+
+            </tbody>
+        </table>
+
+    </div
+@endsection
